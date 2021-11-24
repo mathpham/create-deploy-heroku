0606046e5f789eb134a53836b4722fc172973a22
mathpham <43166622+mathpham@users.noreply.github.com>
2021-11-24 21:39:51 +0900

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="display-1">
+                    Welcome to Home Page!
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
